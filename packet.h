379565c27a6039301a1e5f127be3d4152f2ed2bd
Liz Rice <liz@lizrice.com>
2021-11-17 09:41:57 +0000

    Create packet.h

---
+++ packet.h
@@ -0,0 +1,21 @@
+#include <linux/ip.h>
+
+// Returns the protocol byte for an IP packet, 0 for anything else
+static __always_inline u64 lookup_protocol(struct xdp_md *ctx)
+{
+    u64 protocol = 0;
+
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+    struct ethhdr *eth = data;
+    if (data + sizeof(struct ethhdr) > data_end)
+        return 0;
+
+    if (bpf_ntohs(eth->h_proto) == ETH_P_IP)
+    {
+        struct iphdr *iph = data + sizeof(struct ethhdr);
+        if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) <= data_end)
+            protocol = iph->protocol;
+    }
+    return protocol;
+}
